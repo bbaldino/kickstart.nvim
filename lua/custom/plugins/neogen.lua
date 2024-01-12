dd394abed382606d60c7934c05ccebdbf56b7ea0
bbaldino <bbaldino@cisco.com>
2024-01-12 13:59:44 -0800

    add custom plugins

---
+++ lua/custom/plugins/neogen.lua
@@ -0,0 +1,9 @@
+return {
+  "danymat/neogen",
+    config = function()
+      require('neogen').setup({ snippet_engine = "luasnip" })
+    end,
+    requires = "nvim-treesitter/nvim-treesitter",
+    -- Uncomment next line if you want to follow only stable versions
+    -- tag = "*"
+}
