d686b2c0b3667c807de0b1868d34237d24dff115
bbaldino <bbaldino@cisco.com>
2024-01-22 20:55:40 -0800

    comment out on_attach code

--- lua/custom/on_attach.lua
+++ lua/custom/on_attach.lua
@@ -1,8 +1,8 @@
-return function(client, bufnr)
-  print("in custom on_attach", client.server_capabilities)
-  if client.server_capabilities.inlayHintProvider then
-    print("inlay hints avaiable")
-    -- Not loading correctly...https://github.com/neovim/nvim-lspconfig/issues/2876?
-    -- vim.lsp.inlay_hint(0, true)
-  end
+return function(_client, _bufnr)
+  -- print("in custom on_attach", client.server_capabilities)
+  -- if client.server_capabilities.inlayHintProvider then
+  --   print("inlay hints avaiable")
+  --   -- Not loading correctly...https://github.com/neovim/nvim-lspconfig/issues/2876?
+  --   -- vim.lsp.inlay_hint(0, true)
+  -- end
 end
