83492139d921147d57ff5d1e745b4e51b91e5c50
bbaldino <bbaldino@cisco.com>
2024-01-22 20:56:05 -0800

    add rust struct snippet

--- luasnippets/rust.lua
+++ luasnippets/rust.lua
@@ -1,4 +1,5 @@
 local ls = require("luasnip")
+local s = ls.snippet;
 local t = ls.t;
 local i = ls.i;
 local c = ls.c;
@@ -6,20 +7,34 @@ local fmt = require("luasnip.extras.fmt").fmt
 
 --stylua: ignore
 return {
-	ls.snippet('rustfmt', ls.text_node '#[rustfmt::skip]'),
-	ls.snippet(
-		{ trig = "atest", name = "async test", dscr = "Define an async test" },
-		fmt([[
-		{} {}() {{
-			{}
-		}}
-		]], {
-			c(1, {
-				t({ "#[tokio::test]", "async fn" }),
-				t({ "#[test]", "fn" }),
-			}),
-			i(2, "test_name"),
-			i(3, "")
-		})
-	)
+  s('rustfmt', t '#[rustfmt::skip]'),
+  s({ trig = "test", name = "test", dscr = "Define a test" },
+    fmt([[
+      {} {}() {{
+        {}
+      }}
+    ]], {
+      c(1, {
+        t({ "#[tokio::test]", "async fn" }),
+        t({ "#[test]", "fn" }),
+      }),
+      i(2, "test_name"),
+      i(3, "")
+    })
+  ),
+  s({ trig = "pstruct", name = "public struct", dscr = "Define a public struct" },
+    fmt([[
+      {}
+      pub struct {} {{
+          {}
+      }}
+    ]], {
+      c(1, {
+        t("#[derive(Debug)]"),
+        t("#[derive(Debug, Deserialize, Serialize)]"),
+      }),
+      i(2, "struct name"),
+      i(0, "")
+    })
+  ),
 }
