1b9bfb6a852bd68293d4bad93503eb4f8d90d80d
bbaldino <bbaldino@cisco.com>
2024-02-13 13:55:37 -0800

    ignore snippet completions from lsp

--- init.lua
+++ init.lua
@@ -688,7 +688,16 @@ cmp.setup {
     end, { 'i', 's' }),
   },
   sources = {
-    { name = 'nvim_lsp' },
+    {
+      name = 'nvim_lsp',
+      entry_filter = function(entry, _ctx)
+        -- Ignore snippets from lsp
+        if entry:get_kind() == 15 then
+          return false
+        end
+        return true
+      end,
+    },
     { name = 'luasnip' },
     { name = 'path' },
   },
